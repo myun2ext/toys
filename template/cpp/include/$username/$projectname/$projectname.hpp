ae16ae3cc40c469785f0b9152a6bdd4db1146a75
tomoaki yahagi <yahagi@fact-real.com>
2013-07-24 14:20:43 +0900

    Project template header(2) file add.

---
+++ template/cpp/include/$username/$projectname/$projectname.hpp
@@ -0,0 +1,4 @@
+#ifndef __github_com_$username__$projectname__$projectname_HPP__
+#define __github_com_$username__$projectname__$projectname_HPP__
+
+#endif//__github_com_$username__$projectname__$projectname_HPP__
