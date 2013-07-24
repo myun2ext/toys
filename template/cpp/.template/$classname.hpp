b3829593fe55471097170c27e3fbae0e4b4e3e20
tomoaki yahagi <yahagi@fact-real.com>
2013-07-24 14:25:29 +0900

    .template/$classname.hpp template file

---
+++ template/cpp/.template/$classname.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_$username__$projectname__$path__$classname_HPP__
+#define __github_com_$username__$projectname__$path__$classname_HPP__
+
+namespace $username
+{
+	namespace $projectname
+	{
+  	class $classname
+    {
+    };
+	}
+}
+
+#endif//__github_com_$username__$projectname__$path__$classname_HPP__
